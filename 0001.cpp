3ef054bcaa802747cee8397da0545e8e28da17ec
junes7 <2junes@naver.com>
2021-08-25 08:44:46 +0900

    review how to print string with cout

---
+++ 0001.cpp
@@ -0,0 +1,9 @@
+// 출력하기 01(설명)
+// C/C++언어에서 가장 기본적인 명령이 출력문이다. printf()를 이용해
+// 다음 단어를 출력하시오. Hello
+# include <iostream>
+using namespace std;
+int main(void) {
+    cout<<"Hello"<<endl;
+    return 0;
+}
---
+++ 0002.cpp
@@ -0,0 +1,9 @@
+// 출력하기 02(설명)
+// 이번에는 공백()을 포함한 문장을 출력한다. 다음 문장을 출력해보자.
+// Hello World (대소문자에 주의한다.)
+# include <iostream>
+using namespace std;
+int main() {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
