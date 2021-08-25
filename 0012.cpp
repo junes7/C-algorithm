8f0c6a8521d501a2abaef6ea940de708b3f7bd3b
junes7 <2junes@naver.com>
2021-08-26 07:26:42 +0900

    review how to print one input real number and print two input integers with cin, cout function

---
+++ 0012.cpp
@@ -0,0 +1,12 @@
+// 실수 한 개 입력받아 그대로 출력하기
+// print one input real number
+// 실수형(float)로 변수를 선언하고 그 변수에 실수값을 저장한 후
+// 저장되어 있는 실수값을 출력해보자.
+# include <iostream>
+using namespace std;
+int main() {
+    float f;
+    cin>>f;
+    cout<<f;
+    return 0;
+}
---
+++ 0013.cpp
@@ -0,0 +1,11 @@
+// 정수 두 개 입력받아 그대로 출력하기
+// print two input integers
+// 정수(int) 2개를 입력받아 그대로 출력해보자.
+# include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<a<<" "<<b;
+    return 0;
+}
