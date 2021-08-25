2850aab0431d1b9de90cecb5957fb3bb211caa56
junes7 <2junes@naver.com>
2021-08-25 10:22:02 +0900

    review how to print one input integer and character

---
+++ 0010.cpp
@@ -0,0 +1,12 @@
+// 정수 한 개 입력받아 그대로 출력하기
+// print one input integer
+// 정수형(int)으로 변수를 선언하고, 변수에 정수값을 저장한 후 변수에
+// 저장되어 있는 값을 그대로 출력해보자.
+# include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    cout<<n;
+    return 0;
+}
---
+++ 0011.cpp
@@ -0,0 +1,10 @@
+// 문자 한 개 입력받아 그대로 출력하기
+// print one input character
+# include <iostream>
+using namespace std;
+int main() {
+    char c;
+    cin>>c;
+    cout<<c;
+    return 0;
+}
