331e2246a635423c4155fe080857aea5185b3b2f
junes7 <2junes@naver.com>
2021-08-31 07:20:08 +0900

    solve that print continuously after input one integer and print infinite until input zero problems by using while loop and scanf, printf function

---
+++ 0071.cpp
@@ -0,0 +1,15 @@
+// 정수 입력받아 계속 출력하기
+// print continuously after input one integer
+// n개의 정수가 순서대로 입력된다.
+// -2147483648 ~ +2147483647, 단 n의 최대 개수는 알 수 없다.
+// n개의 입력된 정수를 순서대로 출력해보자.
+// while( ), for( ), do~while( ) 등의 반복문을 사용할 수 없다.
+# include <iostream>
+int main() {
+    int n,t;
+    scanf("%d",&n);
+    reget:
+        scanf("%d",&t);
+        printf("%d\n",t);
+        if(--n!=0) goto reget;
+}
