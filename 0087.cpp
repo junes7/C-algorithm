58fc8e4e9ab7e5f2cc497694217b3b6472e14939
junes7 <2junes@naver.com>
2021-09-01 07:58:42 +0900

    solve that pass multiple of three and list number problems by using for loop, if, else statement, and scanf, printf function

---
+++ 0087.cpp
@@ -0,0 +1,16 @@
+// 3의 배수는 통과?
+// pass multiple of three?
+// 1부터 입력한 정수까지 1씩 증가시켜 출력하는 프로그램을 작성하되,
+// 3의 배수인 경우는 출력하지 않도록 만들어보자. 예를 들면,
+// 1 2 4 5 7 8 10 11 13 14 ...와 같이 출력하는 것이다.
+# include <iostream>
+int main(){
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        if(i%3==0)
+            continue;
+        else
+            printf("%d ",i);
+    }
+}
