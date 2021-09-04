a5f5292b0d47dd4e90ce5334e901cc12bde24e46
junes7 <2junes@naver.com>
2021-09-04 12:21:07 +0900

    solve that fill 2D array in order problems by using nested for loop, and scanf, printf function

---
+++ 1711.cpp
@@ -0,0 +1,18 @@
+// 2차원 배열 순서대로 채우기 3
+// fill 2D array in order
+# include <iostream>
+int main(){
+    int n,i,j,t=1,d[110][110]={};
+    scanf("%d",&n);
+    for(j=1;j<=n;j++){
+        for(i=1;i<=n;i++){
+            d[i][j]=t++;
+        }
+    }
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("%d ",d[i][j]);
+        }
+        printf("\n");
+    }
+}
