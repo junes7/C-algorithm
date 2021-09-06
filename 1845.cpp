318a16e42ce4278a3751eae55fefd0db2417ec95
junes7 <2junes@naver.com>
2021-09-07 07:07:31 +0900

    solve that return character A through function and return integer 1 through function problems by using printf function

---
+++ 1845.cpp
@@ -0,0 +1,10 @@
+// 함수로 문자(char) A 리턴하기
+// return character A through function
+// A 를 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <iostream>
+char f(){
+    return 'A';
+}
+int main(){
+    printf("%c\n",f());
+}
---
+++ 1846.cpp
@@ -0,0 +1,10 @@
+// 함수로 정수(int) 1 리턴하기
+// return integer 1 through function
+// 1 을 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <iostream>
+int f(){
+    return 1;
+}
+int main(){
+    printf("%d\n",f());
+}
