058264cb60a9432fd2ce10fc3aa19bdcae62a367
junes7 <2junes@naver.com>
2021-09-07 07:17:19 +0900

    solve that return long long int -2147483649 through function and return float 3.14 through function problems with printf function

---
+++ 1847.cpp
@@ -0,0 +1,10 @@
+// 함수로 정수(long long int) -2147483649 리턴하기
+// return long long int -2147483649 through function
+// –2147483649 을 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <iostream>
+long long int f(){
+    return -2147483649LL;
+}
+int main(){
+    printf("%lld\n",f());
+}
---
+++ 1848.cpp
@@ -0,0 +1,10 @@
+// 함수로 실수(float) 3.14 리턴하기
+// return float 3.14 through function
+// 3.14 를 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <iostream>
+float f(){
+    return 3.14f;
+}
+int main(){
+    printf("%f\n",f());
+}
