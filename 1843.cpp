9c85c477655ccf7747b8bdf177b28e55b35c80da
junes7 <2junes@naver.com>
2021-09-06 15:44:14 +0900

    solve that print * and ** through function problems by using for loop, and printf function

---
+++ 1843.cpp
@@ -0,0 +1,10 @@
+// 함수로 * 문자 출력하기
+// print * character through function
+// * 을 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <iostream>
+void f(){
+    printf("*");
+}
+int main(){
+    f();
+}
---
+++ 1844.cpp
@@ -0,0 +1,11 @@
+// 함수로 ** 출력하기
+// print ** through function
+// ** 을 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <stdio.h>
+void f(){
+    for(int i=1;i<=2;i++)
+        printf("%c",'*');
+}
+int main(){
+    f();
+}
