d7d620908593e1e3ff0554cb7b42fc2a50610d0c
junes7 <2junes@naver.com>
2021-09-06 14:55:47 +0900

    solve that fill 2D array with snail pattern problems by using nested for loop, 2D array, and scanf, printf function

---
+++ 1738.cpp
@@ -0,0 +1,27 @@
+//  2차원 배열 달팽이 채우기 6
+// fill 2D array with snail pattern
+# include <iostream>
+int main(){
+    int n,m,i,j,b,c,r=-1,l=0;
+    int d[110][110]={0};
+    scanf("%d %d",&n,&m);
+    i=n,j=m+1;
+    for(c=n*m;c>0;){
+        for(b=1;b<=m-l;b++){
+            j+=r;
+            d[i][j]=c--;
+        }
+        for(b=1;b<n-l;b++){
+            i+=r;
+            d[i][j]=c--;
+        }
+        l++;
+        r=-r;
+    }
+    for(i=1;i<=n;i++){
+        for(j=1;j<=m;j++){
+            printf("%d ",d[i][j]);
+        }
+        printf("\n");
+    }
+}
