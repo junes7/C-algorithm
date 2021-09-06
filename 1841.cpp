5c54968e8c345c1159e5b27b80a23fef947a5d86
junes7 <2junes@naver.com>
2021-09-06 15:26:28 +0900

    solve that print string and integer value through function with printf function

---
+++ 1841.cpp
@@ -0,0 +1,10 @@
+// 함수로 hello 문자열 출력하기
+// print hello through function
+// hello를 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <iostream>
+void f(){
+    printf("hello");
+}
+int main(){
+    f();
+}
---
+++ 1842.cpp
@@ -0,0 +1,10 @@
+// 함수로 123 값 출력하기
+// print 123 value through function
+// 123 을 출력하시오. 단, 함수형 문제이므로 함수 f()만 작성하시오.
+# include <iostream>
+void f(){
+    printf("%d",123);
+}
+int main(){
+    f();
+}
