ad519b041968f8e9b266009f84e2d06053bf837c
junes7 <2junes@naver.com>
2021-09-08 10:20:33 +0900

    solve that return integer part only through function and return decimal point only through function problems by using scanf, printf function

---
+++ 1867.cpp
@@ -0,0 +1,13 @@
+// 함수로 정수 부분만 리턴하기
+// return integer part only through function
+// 실수(real number)를 입력 받아 정수 부분만 출력하시오. 단, 함수형
+// 문제이므로 함수 f()만 작성해 제출하시오.
+# include <iostream>
+float x;
+int f(float x){
+    return (int)x;
+}
+int main(){
+    scanf("%f",&x);
+    printf("%d\n",f(x));
+}
