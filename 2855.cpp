f1cd28e31c9c04384a87bf82bf6cd6bc86708b69
junes7 <2junes@naver.com>
2021-09-17 06:56:14 +0900

    solve that practice basic print problems by using printf function

---
+++ 2855.cpp
@@ -0,0 +1,8 @@
+// 기초 출력 연습 1
+// practicde basic print
+// 출력문은 프로그래밍 언어에서 가장 기본적인 명령이다. 다음 문장을
+// 출력하는 프로그램을 작성해보자. Welcome to Koistudy
+# include <iostream>
+int main(){
+    std::cout<<"Welcome to Koistudy";
+}
