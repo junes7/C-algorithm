e0f01441e066812fd3397ba5773db5c1d24ecc04
junes7 <2junes@naver.com>
2021-09-19 01:18:15 +0900

    review how to print string and integer with cpp

---
+++ c++_programming/chapter01/HelloWorld.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int n=20;
+    cout<<"Hello World"<<endl;
+    cout<<"Hello "<<"World"<<endl;
+    cout<<n<<" A "<<3.14<<endl;
+}
---
+++ c++_programming/chapter01/SimpleAdder.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int v1,v2,r;
+    cout<<"첫 번째 숫자입력: ";
+    cin>>v1;
+    cout<<"두 번째 숫자입력: ";
+    cin>>v2;
+    r=v1+v2;
+    cout<<"덧셈결과: "<<r<<endl;
+}
