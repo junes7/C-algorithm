5f3b1c02f6269eda1360ae3a72f0132ec330e736
junes7 <2junes@naver.com>
2021-09-19 03:00:00 +0900

    solve that print input multiplication table and calculate salary problems by using for while loop, and cin,cout function

---
+++ c++_programming/chapter01/Question/Q3.cpp
@@ -0,0 +1,13 @@
+// 숫자를 하나 입력 받아서 그 숫자에 해당하는 구구단을 출력하는
+// 프로그램을 작성해 보자. 예를 들어서 사용자가 5를 입력한다면
+// 구구단에서 5단을 출력해야 한다.
+# include <iostream>
+using namespace std;
+int main(){
+    int i,n;
+    cout<<"구구단 정보 입력: ";
+    cin>>n;
+    for(i=1;i<10;i++){
+        cout<<n<<"X"<<i<<"="<<n*i<<endl;
+    }
+}
