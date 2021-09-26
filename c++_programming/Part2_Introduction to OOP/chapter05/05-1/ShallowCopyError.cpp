e58789a664eb9edbe0952487d699a019c8987dce
junes7 <2junes@naver.com>
2021-09-27 00:56:00 +0900

    study what happened when we implement shallow copy without copy constructor

---
+++ c++_programming/Part2_Introduction to OOP/chapter05/05-1/ShallowCopyError.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class Person{
+private:
+    char *name;
+    int age;
+public:
+    Person(char *myname,int myage):age(myage){
+        name=new char[strlen(myname)+1];
+        strcpy(name,myname);
+    }
+    void ShowPersonInfo(){
+        cout<<"이름: "<<name<<endl;
+        cout<<"나이: "<<age<<endl;
+    }
+    ~Person(){
+        delete []name;
+        cout<<"called destructor!"<<endl;
+    }
+};
+int main(void){
+    Person man1("Lee dong woo",29);
+    Person man2=man1;
+    man1.ShowPersonInfo();
+    man2.ShowPersonInfo();
+}
