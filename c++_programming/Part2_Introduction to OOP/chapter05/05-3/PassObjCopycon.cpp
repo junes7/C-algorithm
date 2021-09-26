e41ec1af35e9e4f1470664c65c05f3c56900b4cf
junes7 <2junes@naver.com>
2021-09-27 01:41:54 +0900

    study how to call copy constructor with class definition, and constructor

---
+++ c++_programming/Part2_Introduction to OOP/chapter05/05-3/PassObjCopycon.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class SoSimple{
+private:
+    int num;
+public:
+    SoSimple(int n):num(n){}
+    SoSimple(const SoSimple &copy):num(copy.num){
+        cout<<"Called SoSimple(const SoSimple &copy)"<<endl;
+    }
+    void ShowData(){
+        cout<<"num: "<<num<<endl;
+    }
+};
+void SimpleFuncObj(SoSimple obj){
+    obj.ShowData();
+}
+int main(void){
+    SoSimple obj(7);
+    cout<<"함수호출 전"<<endl;
+    SimpleFuncObj(obj);
+    cout<<"함수호출 후"<<endl;
+}
