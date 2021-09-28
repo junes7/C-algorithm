168fd24df4ea039ac97bd2c998bc032673be61f1
junes7 <2junes@naver.com>
2021-09-29 00:26:16 +0900

    study how to use static as local variable and global variable

---
+++ c++_programming/Part2_Introduction to OOP/chapter06/06-3/NeedGlobal.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int simObjCnt;
+int cmxObjCnt;
+class SoSimple{
+public:
+    SoSimple(){
+        simObjCnt++;
+        cout<<simObjCnt<<"¹øÂ° SoSimple °´Ã¼"<<endl;
+    }
+};
+class SoComplex{
+public:
+    SoComplex(){
+        cmxObjCnt++;
+        cout<<cmxObjCnt<<"¹øÂ° SoComplex °´Ã¼"<<endl;
+    }
+    SoComplex(const SoComplex &copy){
+        cmxObjCnt++;
+        cout<<cmxObjCnt<<"¹øÂ° SoComplex °´Ã¼"<<endl;
+    }
+};
+int main(void){
+    SoSimple sim1;
+    SoSimple sim2;
+    SoComplex com1;
+    SoComplex com2=com1;
+    SoComplex();
+}
