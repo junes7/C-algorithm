42af22f01067af3ca1a6448d512c87d99bba0c13
junes7 <2junes@naver.com>
2021-09-30 07:35:19 +0900

    study how to do information hiding with class inheritance through University student Information problem

---
+++ c++_programming/Part3_Development of OOP/chapter07/07-2/UnivStudentInheri.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class Person{
+private:
+    char name[50];
+    int age;
+public:
+    Person(char *myname,int myage):age(myage){
+        strcpy(name,myname);
+    }
+    void WhatYourName() const{
+        cout<<"My name is "<<name<<endl;
+    }
+    void HowOldAreYou() const{
+        cout<<"I'm "<<age<<" years old"<<endl;
+    }
+};
+class UnivStudent:public Person{
+private:
+    char major[50];
+public:
+    UnivStudent(char *myname,int myage,char *mymajor):Person(myname,myage){
+        strcpy(major,mymajor);
+    }
+    void WhoAreYou() const{
+        WhatYourName();
+        HowOldAreYou();
+        cout<<"My major is "<<major<<endl<<endl;
+    }
+};
+int main(void){
+    UnivStudent ustd1("Lee",22,"Computer eng.");
+    ustd1.WhoAreYou();
+    UnivStudent ustd2("Yoon",21,"Electronic eng.");
+    ustd2.WhoAreYou();
+}
